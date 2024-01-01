b70ddabc6c5e7bd37f3190ffb7c0f4b626a9d3ca
Nay Ba La <naybala.zotefamily@gmail.com>
2024-01-01 21:27:40 +0630

    img edit/update

--- function/productUpdate.php
+++ function/productUpdate.php
@@ -1,9 +1,20 @@
 <?php
 if (isset($_POST['product-update-btn'])) {
-    $id = $_REQUEST['action'];
+    $oldImage = null;
+    if($_FILES['new_image']['name'] != "") {
+        $imageName = $_FILES['new_image']['name']; //image-1 1.png
+        $folder = "./image/" . $imageName;
+        $imageName = "image/" . $imageName; // image/image-1 1.png
+        $oldImage =  $_POST['old_image'];
+        }else{
+       $imageName = $_POST['old_image'];
+    }
+
+    $id = $_POST['id'];
     $name = $_POST['name'];
     $price = $_POST['price'];
     $description = $_POST['description'];
+   
 
     if ($name == null || $price == null) {
         $errorInfo = true;
@@ -14,12 +25,21 @@ if (isset($_POST['product-update-btn'])) {
     if ($errorInfo == false) {
         $selectDuplicate = mysqli_query($connection, "SELECT * FROM  products WHERE name = '" . $name . "'");
         $checkDuplicate = mysqli_num_rows($selectDuplicate);
-        if ($checkDuplicate > 0) {
+        if ($checkDuplicate > 1) {
 
         } else {
-            $insertProduct = mysqli_query($connection,
-                "UPDATE products SET name='" . $name . "', price='" . $price . "',description='" . $description . "' WHERE id='" . $id . "'");
-            if ($insertProduct) {
+            $updateProduct = mysqli_query($connection,
+                "UPDATE products SET 
+                name='" . $name . "',
+                price='" . $price . "',
+                image='" . $imageName . "',
+                description='" . $description . "' 
+                WHERE id='" . $id . "'");
+            if ($updateProduct) {
+                if($_FILES['new_image']['name'] != "") {
+                    unlink("./".$oldImage);
+                    move_uploaded_file($_FILES['new_image']['tmp_name'], $folder);
+                }
                 echo "<script>window.location.href='index.php?page=product-page'</script>";
             }
         }
--- ui/Product/edit.php
+++ ui/Product/edit.php
@@ -4,22 +4,53 @@ include 'function/productUpdate.php';
 ?>
 <h5>This is Product Edit Page</h5>
 <div class="container border p-5">
-    <form action="" method='POST'>
+    <form action="" method='POST' enctype="multipart/form-data">
+        <!-- id -->
+        <input type="hidden" name="id" value="<?php echo @$editProduct['id'] ?>">
+        <!-- id -->
+
+        <!-- name -->
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Product Name</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" name="name"
                 value="<?php echo @$editProduct['name'] ?>">
         </div>
+        <!-- name -->
+
+        <!-- image -->
+        <div class="mb-3 shadow-lg p-5">
+            <label for="exampleFormControlInput1" class="form-label">Product image</label>
+
+            <input type="hidden" class="form-control" id="exampleFormControlInput1" name="old_image"
+                value="<?php echo (@$editProduct["image"]) ?>">
+
+            <input type="file" class="form-control" id="exampleFormControlInput1" name="new_image">
+
+            <br>
+            <!-- For Show -->
+            <div class="text-center">
+                <img src="<?php echo (@$editProduct["image"]) ?>" class="img-thumbnail w-25">
+            </div>
+            <!-- For Show -->
+        </div>
+        <!-- image -->
+
+        <!-- price -->
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Product Price</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" name="price"
                 value="<?php echo @$editProduct['price'] ?>">
         </div>
+        <!-- price -->
+
+        <!-- description -->
         <div class=" mb-3">
             <label for="exampleFormControlInput1" class="form-label">Product Description</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" name="description"
                 value="<?php echo @$editProduct['description'] ?>"></input>
         </div>
+        <!-- description -->
+
         <br><br>
         <div class="d-flex float-end gap-5">
             <a href="index.php?page=product-page">
